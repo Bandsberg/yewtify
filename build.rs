ce29ce6370df330902f3bf075e50e75f9ca37272
Bandsberg <rodekassen@bandsberg.dk>
2020-08-16 08:46:09 +0200

    added grass build dependency & messing aorund

--- build.rs
+++ build.rs
@@ -1,15 +1,19 @@
 use anyhow::{anyhow, Error};
-use sass_rs::{compile_file, Options, OutputStyle};
+//use sass_rs::{compile_file, Options, OutputStyle};
 use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
+use grass;
 
 fn main() -> Result<(), Error> {
-    let mut opts = Options::default();
-    opts.output_style = OutputStyle::Compact;
-    let data = compile_file("./styles/yewtify.sass", opts)
-        .map_err(|err| anyhow!("sass builder failed with: {}", err))?;
+    //let mut opts = Options::default();
+    //opts.output_style = OutputStyle::Compact;
+    //let data = compile_file("./styles/yewtify.sass", opts)
+    //    .map_err(|err| anyhow!("sass builder failed with: {}", err))?;
+    
+    let data = grass::from_path("./styles/yewtify.sass", &grass::Options::default())?;
+    
     let mut path = PathBuf::from(env::var("YEWTIFY_OUT")?);
     path.push("yewtify.css");
     let mut file = File::create(path)?;
